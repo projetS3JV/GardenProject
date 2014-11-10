f483bd5d4aedadcd28966d845408059c8e6df256
brate001 <brate001@pc066.iut-rcc-info.urca>
2014-11-10 11:54:22 +0100

    @brateau_etienne
    Création des classes d'interface graphique.

---
+++ src/jardin/ui/MainFrame.java
@@ -0,0 +1,10 @@
+package jardin.ui;
+
+public class MainFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/jardin/ui/OutilPanel.java
@@ -0,0 +1,5 @@
+package jardin.ui;
+
+public class OutilPanel {
+
+}
