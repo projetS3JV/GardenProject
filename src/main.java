1ade8e25e7510fcfe5925d7bca210a3a5b0e36da
Thibault Fontanelle <thibault.fontanelle@etudiant.univ-reims.fr>
2014-11-10 11:21:10 +0100

    C'est pas nous qui avons fait ça

--- src/main.java
+++ src/main.java
@@ -1,9 +1,14 @@
 
 public class main {
-	public void thibaultJason(){ System.out.println("Vive les poneys");}
+	
 	public static void main(String[] args) {
 
 	}
+	/*
+	 * L'équipe des sexy boys au swagg de poule!
+	 * Et puis on aime les raviolis.
+	 */
+	public void thibaultJason(){ System.out.println("Vive les poneys");}
 	
 	public void remyBenjamin(){
 		
