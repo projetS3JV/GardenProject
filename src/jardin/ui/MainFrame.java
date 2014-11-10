512678a07ef4b38ef1e3c17c5eafea3704607b1d
Thibault Fontanelle <thibault.fontanelle@etudiant.univ-reims.fr>
2014-11-10 12:14:05 +0100

    @CABANAC-FONTANELLE

--- src/jardin/ui/MainFrame.java
+++ src/jardin/ui/MainFrame.java
@@ -1,10 +1,45 @@
 package jardin.ui;
 
-public class MainFrame {
+import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class MainFrame extends JFrame{
+	
+	private JardinPanel jardinPanel;
+	private OutilPanel outilPanel;
+	private Plantotheque plantotheque;
+	private CalendarPanel calendarPanel;
+	private MenuPanel menuPanel;
+	
+	private MainFrame(){
+		this.setSize(900, 600);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		/*
+		this.add(northButton, BorderLayout.NORTH);
+
+	   
+	    this.add(southButton, BorderLayout.SOUTH);
+
+	    
+	    this.add(eastButton, BorderLayout.EAST);
+
+	    
+	    this.add(westButton, BorderLayout.WEST);
+
+	    
+	    this.add(centerButton, BorderLayout.CENTER);*/
+
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SwingUtilities.invokeLater(() -> {
+			MainFrame m = new MainFrame();
+			m.setVisible(true);
+		});
+		
 	}
 
 }
