14d2304c96f538f5796471eeb7d8d564fb93e71f
Felix Robinet <felix.robinet@gmail.com>
2014-11-10 11:23:12 +0100

    @robinet_felix

--- src/main.java
+++ src/main.java
@@ -4,21 +4,5 @@ public class main {
 	public static void main(String[] args) {
 
 	}
-	/*
-	 * L'équipe des sexy boys au swagg de poule!
-	 * Et puis on aime les raviolis.
-	 */
-	public void thibaultJason(){ System.out.println("Vive les poneys");}
-	
-	public void remyBenjamin(){
-		
-	}
 
-	public void felix() {
-		
-	}
-	
-	public void etienne() {
-		
-	}
 }
