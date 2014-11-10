b6b75439a43176ee2122174d0819e69d34080e92
Remy BOSQUART <remy.bosquart@etudiant.univ-reims.fr>
2014-11-10 12:07:11 +0100

    @remy_bosquart

--- src/jardin/plante/Plante.java
+++ src/jardin/plante/Plante.java
@@ -1,5 +1,70 @@
 package jardin.plante;
 
-public class Plante {
+import java.util.GregorianCalendar;
 
+public class Plante {
+	
+	/*
+	 * La taille finale de la plante
+	 */
+	private int tailleFin;
+	
+	/*
+	 * La date de la floraison
+	 */
+	private GregorianCalendar dateFloraison;
+	
+	/*
+	 * La date de plantation
+	 */
+	private GregorianCalendar datePlantation;
+	
+	/*
+	 * La couleur de la plante
+	 */
+	private String couleur;
+	
+	/*
+	 * La date de la derniére taille
+	 */
+	private GregorianCalendar dateTaille;
+	
+	/* 
+	 * True si la plante est vivace
+	 */
+	private boolean vivace;
+	
+	
+	/*
+	 * Nom de la plante
+	 */
+	private String nom;
+	
+	/*
+	 * Nom latin de la plante
+	 */
+	private String nomL;
+	
+	/*
+	 * True si la plante est persistante
+	 */
+	private boolean persistant;
+	
+	/*
+	 * L'image de la plante fleurie
+	 */
+	private String ImgFleurie;
+	
+	/*
+	 * L'image de la fleur non fleurie
+	 */
+	private String ImgNonFleurie;
+	
+	
+	/*
+	 * Durée du développement en jour
+	 */
+	private int DureeDev;
+	
+	
 }
