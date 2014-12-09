1db637888f9b506c710036a3991b42914e85e2ec
Thibault <Thibault@Thibault-Portable>
2014-12-09 11:59:48 +0100

    @Fontanelle_Thibault Modification de PlantePanel, Plantotheque en developpement.

--- src/jardin/ui/PlantePanel.java
+++ src/jardin/ui/PlantePanel.java
@@ -9,17 +9,17 @@ import javax.swing.JSplitPane;
 
 public class PlantePanel extends JPanel {
 	private JPanel plantePanel;
-	private int id;
+	private AccesBD instance = AccesBD.getInstance();
 	
-	public PlantePanel() {
+	public PlantePanel(int id) {
 		plantePanel = new JPanel();
 		
 		//Récupération de l'image de la plante.
-		ImageIcon img = AccesBD.getInstance().getPlante(id).getImgFleurie();
+		ImageIcon img = instance.getPlante(id).getImgFleurie();
 		
 		//Création des labels pour l'affichage du nom et la description des plantes.
-		JLabel name = new JLabel("Nom : " + AccesBD.getInstance().getPlante(id).getNom());
-		JLabel desc = new JLabel("Description : " + AccesBD.getInstance().getPlante(id).getDescription());
+		JLabel name = new JLabel("Nom : " + instance.getPlante(id).getNom());
+		JLabel desc = new JLabel("Description : " + instance.getPlante(id).getDescription());
 		//Label contenant l'image de la plante.
 		JLabel image = new JLabel();
 		image.setIcon(img);
--- src/jardin/ui/Plantotheque.java
+++ src/jardin/ui/Plantotheque.java
@@ -1,14 +1,51 @@
 package jardin.ui;
 
 import java.awt.GridLayout;
-import jardin.AccesBD;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Plantotheque extends JPanel {
-	JPanel planto = new JPanel();
-	int nb = AccesBD.getPlantes().size();
+import jardin.AccesBD;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+
+public class Plantotheque extends JFrame {
+	JFrame planto;
+	AccesBD instance = AccesBD.getInstance();
 	
-	
-	
-	//GridLayout grillePlantes = new GridLayout();
+	public Plantotheque() {
+		planto = new JFrame();
+		
+		//Création de la grille contenant le descriptif des plantes en fonction du nombre de la plante.
+		int nbPlante = instance.getPlantes().size();
+		GridLayout grillePlantes = new GridLayout((nbPlante/2) + 1, 2, 20, 10);
+		
+		//Remplissage du gridLayout avec les instance de PlantePanel.
+		for(int i = 0; i < nbPlante; i++) {
+			PlantePanel tmpi = new PlantePanel(i);
+			grillePlantes.addLayoutComponent(instance.getPlante(i).getNom(), tmpi);
+		}
+		
+		//Création de la barre de défilement et des boutons Ajouter et Annuler.
+		JScrollBar scrollbare = new JScrollBar();
+		JButton ajouter = new JButton("Ajouter");
+		JButton annuler = new JButton("Annuler");
+		//Ajout des actionListener sur les boutons.
+		/*
+		ajouter.addActionListener(new ActionListener() {
+			//@Override
+			public void actionPerformed(ActionEvent e) {
+				instance.insertPlante(instance.getPlante(id));
+			}
+		});
+		*/
+		annuler.addActionListener(new ActionListener() {
+			//@Override
+			public void actionPerformed(ActionEvent e) {
+				Plantotheque.this.planto.setVisible(false);
+			}
+		});;
+	}
 }
