7013c317d8c4d33b7204fc51278357491dc708b8
Thibault <Thibault@Thibault-Portable>
2014-12-15 12:13:39 +0100

    @Fontanelle_Thibault Avancement de Plantotheque et PlantePanel

--- src/jardin/ui/PlantePanel.java
+++ src/jardin/ui/PlantePanel.java
@@ -1,33 +1,48 @@
 package jardin.ui;
 
+import java.awt.Component;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import jardin.AccesBD;
+import jardin.plante.Plante;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
+import javax.swing.ListCellRenderer;
 
-public class PlantePanel extends JLabel {
+public class PlantePanel extends JLabel implements ListCellRenderer{
 	private JPanel plantePanel;
-	private AccesBD instance = AccesBD.getInstance();
+	private JLabel image = new JLabel();
+	private JLabel name, desc;
 	
-	public PlantePanel(int id) {
+	public PlantePanel() {
 		plantePanel = new JPanel();
-		
-		//Récupération de l'image de la plante.
-		ImageIcon img = instance.getPlante(id).getImgFleurie();
-		
-		//Création des labels pour l'affichage du nom et la description des plantes.
-		JLabel name = new JLabel("Nom : " + instance.getPlante(id).getNom());
-		JLabel desc = new JLabel("Description : " + instance.getPlante(id).getDescription());
-		//Label contenant l'image de la plante.
-		JLabel image = new JLabel();
-		image.setIcon(img);
 		
 		JSplitPane split = new JSplitPane();
 	    split.setLeftComponent(image);
 	    split.setRightComponent(name);
 	    split.setRightComponent(desc);
 	    plantePanel.add(split);
+	}
+
+	@Override
+	public Component getListCellRendererComponent(JList list, Object value,
+			int index, boolean isSelected, boolean cellHasFocus) {
+		
+		Plante p = (Plante) value;
+		//Récupération de l'image de la plante.
+		ImageIcon img = p.getImgFleurie();
+		
+		//Création des labels pour l'affichage du nom et la description des plantes.
+		name = new JLabel("Nom : " + p.getNom());
+		desc = new JLabel("Description : " + p.getDescription());
+		
+		//Label contenant l'image de la plante.
+		image.setIcon(img);
+		return this;
 	}
 }
