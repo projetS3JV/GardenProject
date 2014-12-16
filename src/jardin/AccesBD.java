eb71b6757ed5be5a4b71fba58e20a145baef06dd
Remy BOSQUART <remy.bosquart@etudiant.univ-reims.fr>
2014-12-16 11:14:29 +0100

    @Bosquart Remy Mise a jour des couleur, des date et tout et tout Parce que ya Felix qui me fait rechanger toute les 10min mon code !!!!!!!!

--- src/jardin/AccesBD.java
+++ src/jardin/AccesBD.java
@@ -9,10 +9,12 @@ import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -20,6 +22,8 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import javax.swing.ImageIcon;
+
+import org.hsqldb.jdbc.JDBCArrayBasic;
 
 public final class AccesBD {
 
@@ -102,7 +106,7 @@ public final class AccesBD {
 	 * Insertion d'une plante dans la base de donnée
 	 */
 	public void insertPlante(Plante p) {
-		String sql = "INSERT INTO PLANTE VALUES (null, ?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		String sql = "INSERT INTO PLANTE VALUES (null, ?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 		this.planteList.add(p);
 		try {
 			PreparedStatement stat = this.connection.prepareStatement(sql);
@@ -112,13 +116,14 @@ public final class AccesBD {
 			stat.setInt(4, p.getTailleFin());
 			stat.setInt(5, p.getEnsoleillement());
 			stat.setString(6, escape("p.getImgFleurie()"));
-			stat.setString(7, escape("p.getImgNonFleurie()"));
-			stat.setInt(8, p.getCouleur().getRGB());
+			stat.setInt(7, p.getCouleur_en_fleur().getRGB());
+			stat.setInt(8, p.getCouleur_non_fleuris().getRGB());
 			stat.setInt(9, p.getTypeSol());
 			stat.setDate(10, (Date) p.getDatePlantation());
 			stat.setDate(11, (Date) p.getDateFloraison());
-			stat.setBoolean(12, p.isVivace());
-			stat.setString(13, escape(p.getDescription()));
+			stat.setDate(12, (Date) p.getDateFinFloraison());
+			stat.setBoolean(13, p.isVivace());
+			stat.setString(14, escape(p.getDescription()));
 			stat.executeUpdate();
 		} catch (SQLException e1) {
 			e1.printStackTrace();
@@ -130,8 +135,8 @@ public final class AccesBD {
 		try {
 			PreparedStatement stat = this.connection.prepareStatement(sql);
 			stat.setInt(1, z.getId());
-			// stat.set(2, z.xpoints);
-			// stat.set(3, z.ypoints);
+			stat.setArray(2,intArrayToJDBXArray(z.xpoints));
+			stat.setArray(3, intArrayToJDBXArray(z.ypoints));
 			stat.setInt(4, z.getEnsoleillement());
 			stat.executeUpdate();
 		} catch (SQLException e) {
@@ -145,8 +150,8 @@ public final class AccesBD {
 			PreparedStatement stat = this.connection.prepareStatement(sql);
 			stat.setInt(1,z.getPlante().getId());
 			stat.setInt(2,z.getId()); 
-			// stat.setArray(3,z.xpoints);
-			// stat.setInt(4, z.ypoints);
+			stat.setArray(3,intArrayToJDBXArray(z.xpoints));
+			stat.setArray(4,intArrayToJDBXArray(z.ypoints));
 			stat.setInt(5, z.getTypeSol());
 			stat.setInt(6, z.getEnsoleillement());
 			stat.executeUpdate();
@@ -156,10 +161,12 @@ public final class AccesBD {
 	}
 
 	public void insertJardin(Jardin j) {
-		String sql = "INSERT INTO JARDIN VALUES(null,?)";
+		String sql = "INSERT INTO JARDIN VALUES(null,?,?,?)";
 		try {
 			PreparedStatement stat = this.connection.prepareStatement(sql);
 			stat.setString(1, escape(j.getName()));
+			stat.setInt(2, j.getWidth());
+			stat.setInt(3, j.getHeight());
 			stat.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -168,7 +175,7 @@ public final class AccesBD {
 
 	public void updatePlante(Plante p) throws IllegalArgumentException {
 		//if (p.getId() != -1) {
-			String sql = "UPDATE PLANTE SET nom_Usuel =?, nom_Latin = ?, type_Plante = ?, hauteur = ?, luminosite = ?, url_Image_En_Fleur = ?, url_Image_Sans_Fleur = ?, couleur_RGB = ?, type_Sol = ?,	date_Debut = ?,	date_Fin = ?, vivace = ?, description = ? WHERE id="
+			String sql = "UPDATE PLANTE SET nom_Usuel =?, nom_Latin = ?, type_Plante = ?, hauteur = ?, luminosite = ?,url_Image_En_Fleur=?, couleur_en_fleur = ?, couleur_non_fleuris = ?, type_Sol = ?,	date_Plantation = ?,	date_DebutFloraison = ?, date_FinFloraison= ?, vivace = ?, description = ? WHERE id="
 					+ p.getId();
 			try {
 				PreparedStatement stat = this.connection.prepareStatement(sql);
@@ -178,13 +185,14 @@ public final class AccesBD {
 				stat.setInt(4, p.getTailleFin());
 				stat.setInt(5, p.getEnsoleillement());
 				stat.setString(6, escape("p.getImgFleurie()"));
-				stat.setString(7, escape("p.getImgNonFleurie()"));
-				stat.setInt(8, p.getCouleur().getRGB());
+				stat.setInt(7, p.getCouleur_en_fleur().getRGB());
+				stat.setInt(8, p.getCouleur_non_fleuris().getRGB());
 				stat.setInt(9, p.getTypeSol());
 				stat.setDate(10, (Date) p.getDatePlantation());
 				stat.setDate(11, (Date) p.getDateFloraison());
-				stat.setBoolean(12, p.isVivace());
-				stat.setString(13, escape(p.getDescription()));
+				stat.setDate(12, (Date) p.getDateFinFloraison());
+				stat.setBoolean(13, p.isVivace());
+				stat.setString(14, escape(p.getDescription()));
 				stat.executeUpdate();
 			} catch (SQLException e1) {
 				e1.printStackTrace();
@@ -202,8 +210,8 @@ public final class AccesBD {
 			try {
 				PreparedStatement stat = this.connection.prepareStatement(sql);
 				stat.setInt(1, z.getId());
-				// stat.set(2, z.xpoints);
-				// stat.set(3, z.ypoints);
+				stat.setArray(2, intArrayToJDBXArray(z.xpoints));
+				stat.setArray(3, intArrayToJDBXArray(z.ypoints));
 				stat.setInt(4, z.getEnsoleillement());
 				stat.executeUpdate();
 			} catch (SQLException e) {
@@ -222,8 +230,8 @@ public final class AccesBD {
 				PreparedStatement stat = this.connection.prepareStatement(sql);
 				stat.setInt(1,z.getPlante().getId());
 				stat.setInt(2,z.getId());
-				// stat.setArray(3,z.xpoints);
-				// stat.setInt(4, z.ypoints);
+				stat.setArray(3,intArrayToJDBXArray(z.xpoints));
+				stat.setArray(4, intArrayToJDBXArray(z.ypoints));
 				stat.setInt(5, z.getTypeSol());
 				stat.setInt(6, z.getEnsoleillement());
 				stat.executeUpdate();
@@ -237,10 +245,12 @@ public final class AccesBD {
 
 	public void updateJardin(Jardin j) throws IllegalArgumentException {
 		if (j.getId() != -1) {
-			String sql = "UPDATE JARDIN SET name=? WHERE id ="+j.getId();
+			String sql = "UPDATE JARDIN SET name=?,width = ?,height= ? WHERE id ="+j.getId();
 			try {
 				PreparedStatement stat = this.connection.prepareStatement(sql);
 				stat.setString(1, escape(j.getName()));
+				stat.setInt(2, j.getWidth());
+				stat.setInt(3, j.getHeight());
 				stat.executeUpdate();
 			} catch (SQLException e) {
 				e.printStackTrace();
@@ -316,19 +326,52 @@ public final class AccesBD {
 	public static void main(String[] args) {
 		AccesBD bd = AccesBD.getInstance();
 		Plante p = new Plante(10, new Date(datee(2010, Calendar.FEBRUARY, 15)),
-				new Date(datee(2010, Calendar.AUGUST, 15)), Color.blue, true,
-				"popol", "popolus patatus", new ImageIcon("/bla/img1.png"),
-				new ImageIcon("/bla/img2.png"), TypePlante.FLEUR,
+				new Date(datee(2010, Calendar.AUGUST, 15)),new Date(datee(2010, Calendar.FEBRUARY, 20)), Color.blue, Color.black, true,
+				"popol", "popolus patatus", new ImageIcon("/bla/img1.png"), TypePlante.FLEUR,
 				Ensoleillement.SOLEIL, TypeSol.LIMONEUX,
 				"c'est une zolie fleur");
-		//bd.insertPlante(p);
-		//bd.updatePlante(p);
+		bd.insertPlante(p);
+		bd.updatePlante(p);
+		//bd.deletePlante(0);
 		
-		bd.deletePlante(7);
+		//Zone z = new Zone(5, 10, 10);
+		//bd.insertZone(z);
+		
+		Jardin j =new Jardin("Ma maison",100,100);
+		bd.insertJardin(j);
+		bd.deleteJardin(0);
+		
 		bd.close();
+	}
+
+	//-----------------------------------------------------------------------------------------
+	
+	public static int[] JDBCArrayTointArray(Array T) {
+		ResultSet rs;
+		ArrayList<Integer> x = new ArrayList<Integer>();
+		try {
+			rs = T.getResultSet();
+			while (rs.next()) 
+				x.add(rs.getInt(1));	
+		} catch (SQLException e) {e.printStackTrace();}
+		int[] ret = new int[x.size()];
+		for (int i = 0 ; i < x.size() ; i++) {
+			ret[i] = x.get(i);
+		}
+		return ret;
+	}
+	
+	
+	public static JDBCArrayBasic intArrayToJDBXArray(int[] T) {
+		org.hsqldb.types.Type type = org.hsqldb.types.Type.SQL_INTEGER;
+		Object[] o = new Object[T.length];
+		for (int i = 0 ; i< T.length ; i++) 
+			o[i] = new Integer(T[i]);
+		return new JDBCArrayBasic(o, type);
 	}
 
 	
 	// Pense bête --------------->
 	// Reprendre les id lors des updates
+	// Gerer les le sortedlist
 }
