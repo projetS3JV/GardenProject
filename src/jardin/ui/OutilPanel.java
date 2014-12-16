3c2503b0d359aec3f23fe7743cec1a0cdc75d25b
caban001 <caban001@pc035.iut-rcc-info.urca>
2014-12-16 11:16:20 +0100

    @CABANAC_Jason début de outilPanel

--- src/jardin/ui/OutilPanel.java
+++ src/jardin/ui/OutilPanel.java
@@ -1,7 +1,11 @@
 package jardin.ui;
 
+import java.awt.Component;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -14,8 +18,9 @@ public class OutilPanel extends JPanel{
 		this.setLayout(new GridLayout( 2,1));
 		
 		//Création du pannel qui va recevoir les boutons déclenchant les outils
-		JButton tracer = new JButton("Tracer zone"
-		this.outilsPanel
+		JButton tracer = new JButton("Tracer zone", new ImageIcon());
+	
+		//this.outilsPanel
 		
 		
 	}
