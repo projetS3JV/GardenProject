675cd6cc3978367b9c3e2e035023a0bc09728ffc
Felix Robinet <felix.robinet@gmail.com>
2015-01-05 09:21:01 +0100

    corrigé

--- src/jardin/ui/PlantePanel.java
+++ src/jardin/ui/PlantePanel.java
@@ -3,6 +3,7 @@ package jardin.ui;
 import jardin.plante.Plante;
 
 import java.awt.Component;
+import java.awt.FlowLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -11,27 +12,21 @@ import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.ListCellRenderer;
 
-public class PlantePanel extends JLabel implements ListCellRenderer{
+public class PlantePanel extends JPanel implements ListCellRenderer{
 	
 	private static final long serialVersionUID = 1L;
 	private JPanel plantePanel;
-	private JLabel image = new JLabel();
+	private JLabel image;
 	private JLabel name, desc;
 	
 	public PlantePanel() {
 		super();
-		plantePanel = new JPanel();
-		plantePanel.setSize(100, 50);
-		
-		JSplitPane split = new JSplitPane();
-	    split.setLeftComponent(image);
-	    split.setRightComponent(name);
-	    split.setRightComponent(desc);
-	    plantePanel.add(split);
-	    this.add(plantePanel);
+		name = new JLabel();
+		desc = new JLabel();
+		image = new JLabel();
 	}
 
-	//@Override
+	@Override
 	public Component getListCellRendererComponent(JList list, Object value,
 			int index, boolean isSelected, boolean cellHasFocus) {
 		
@@ -43,11 +38,15 @@ public class PlantePanel extends JLabel implements ListCellRenderer{
 		//Creation des labels pour l'affichage du nom et la description des plantes.
 		//name = new JLabel("Nom : " + p.getNom());
 		//desc = new JLabel("Description : " + p.getDescription());
-		name = new JLabel("Nom : RaindowDash");
-		desc = new JLabel("Description : Poney");
+		name.setText(p.getNom());
+		desc.setText(p.getDescription());
 		
 		//Label contenant l'image de la plante.
 		image.setIcon(img);
+		this.setLayout(new FlowLayout());
+		this.add(image);
+		this.add(name);
+		this.add(desc);
 		return this;
 	}
 	
