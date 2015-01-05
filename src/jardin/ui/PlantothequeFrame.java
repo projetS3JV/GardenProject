be943e4c79050d0be7f54e395378e7d621821a82
Thibault <Thibault@Thibault-Portable>
2015-01-05 12:09:36 +0100

    @Fontanelle_Thibault

--- src/jardin/ui/PlantothequeFrame.java
+++ src/jardin/ui/PlantothequeFrame.java
@@ -40,21 +40,6 @@ public class PlantothequeFrame extends JFrame {
 		//Creation de la grille contenant le descriptif des plantes en fonction du nombre de la plante.
 		int nbPlante = instance.getPlantes().getSize();
 		System.out.print(nbPlante);
-		for(int i = 0; i < nbPlante; i++) {
-			Plante Ptmp = modelList.getElementAt(i);
-			//modelList.add(Ptmp);
-		}
-		/*
-		Plante p = new Plante(10, null,null,null, Color.blue, Color.black, true,
-				"Chaise", "popolus patatus", new ImageIcon("res/Img/test.png"), TypePlante.FLEUR,
-				Ensoleillement.SOLEIL, TypeSol.LIMONEUX,
-				"c'est une zolie fleur ololololololololololololololololol");
-		Plante p1 = new Plante(10, null,null,null, Color.blue, Color.black, true,
-				"Tartiflette", "Tartifletus Patatus", new ImageIcon("res/Img/test.png"), TypePlante.FLEUR,
-				Ensoleillement.SOLEIL, TypeSol.LIMONEUX,
-				"c'est une zolie fleur bleuableuzbleuavleuzezkjfvjakzr");
-		*/
-		
 		
 		listePlante = new JList<Plante>(modelList);
 		listePlante.setCellRenderer(new PlantePanel());
