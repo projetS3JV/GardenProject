6336c76809b64f3bb220991b9fe760a4d56d2970
Thibault <Thibault@Thibault-Portable>
2015-01-09 16:07:20 +0100

    @Fontanelle_Thibault : Modification de PlantothequePanel

--- src/jardin/ui/PlantothequePanel.java
+++ src/jardin/ui/PlantothequePanel.java
@@ -1,19 +1,18 @@
 package jardin.ui;
 
 import jardin.AccesBD;
-import jardin.Ensoleillement;
 import jardin.SortedListModel;
-import jardin.TypeSol;
 import jardin.plante.Plante;
-import jardin.plante.TypePlante;
+import jardin.zone.AbstractZone;
+import jardin.zone.ZonePlantable;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -27,47 +26,55 @@ public class PlantothequePanel extends JPanel {
 	private SortedListModel modelList = instance.getPlantes();
 	
 	public PlantothequePanel() {
-		this.setSize(800, 600);
+		this.setSize(400, 400);
 		this.setLayout(new BorderLayout());
 		
 		
-		//Cr�ation de la grille contenant le descriptif des plantes en fonction du nombre de la plante.
-		int nbPlante = instance.getPlantes().getSize();
-		
-		Plante p = new Plante(10, null,null,null, Color.blue, Color.black, true,
-				"popol", "popolus patatus", new ImageIcon("res/Img/test.png"), TypePlante.FLEUR,
-				Ensoleillement.SOLEIL, TypeSol.LIMONEUX,
-				"c'est une zolie fleur");
-		modelList.add(p);
-		
+		//Creation de la grille contenant le descriptif des plantes en fonction du nombre de la plante.
 		listePlante = new JList<Plante>(modelList);
 		listePlante.setCellRenderer(new PlantePanel());
 		this.add(new JScrollPane(this.listePlante));
 
 		
-		//Cr�ation des boutons Ajouter et Annuler.
+		//Creation de la barre de defilement et des boutons Ajouter et Annuler.
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new FlowLayout());
 		JButton ajouter = new JButton("Ajouter");
-		this.add(ajouter, BorderLayout.SOUTH);
+		JButton fermer = new JButton("Fermer");
+		buttons.add(ajouter);
+		buttons.add(fermer);
+		this.add(buttons, BorderLayout.SOUTH);
 		
 		//Ajout des actionListener sur les boutons.
 		ajouter.addActionListener(new ActionListener() {
 			//@Override
 			public void actionPerformed(ActionEvent e) {
-				//R�cup�rer la zone s�lectionn�
-				//ZonePlantable.setPlante(select);
+				AbstractZone zone = MainFrame.getInstance().getJardinPanel().getSelected();
+				((ZonePlantable) zone).setPlante(select);
 			}
 		});
+		
+		fermer.addActionListener(new ActionListener() {
+			//@Override
+			public void actionPerformed(ActionEvent e) {
+				PlantothequePanel.this.setVisible(false);
+			}
+		});
+		
+		//this.pack();
 	}
 	
 	/**
-	 * M�thode mettant en m�moire la plante s�lectionn�e.
+	 * Methode ettant en memoire la plante si selectionne
 	 */
 	public void setSelected(PlantePanel plantePanel, int id) {
 		select = (Plante) this.listePlante.getSelectedValue();
 	}
 	
 	public static void main(String arg[]) {
+		JFrame frame = new JFrame();
 		PlantothequePanel p = new PlantothequePanel();
+		frame.add(p);
 		p.setVisible(true);
 	}
 	
