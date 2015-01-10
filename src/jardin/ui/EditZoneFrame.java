ab8e6c7c4169f68d72a75cd06a9bb4fd6da594fb
brate001 <etienne.brateau@etudiant.univ-reims.fr>
2015-01-10 10:24:17 +0100

    @EB

--- src/jardin/ui/EditZoneFrame.java
+++ src/jardin/ui/EditZoneFrame.java
@@ -1,51 +1,32 @@
 package jardin.ui;
 
-import jardin.zone.AbstractZone;
-
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class EditZoneFrame extends JDialog{
 private static final long serialVersionUID = 1L;
 	
 	private String[] res = null;
 
-	private EditZoneFrame(JFrame f, AbstractZone z) {
-		super(f, "Nouveau jardin", true);
+	private EditZoneFrame(JFrame f) {
+		super(f, "ModifierZone", true);
 		this.setSize(230, 200);
 		this.setResizable(false);
 		this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 		
-		JLabel largeur = new JLabel("largeur  :");
-		JLabel hauteur = new JLabel("hauteur :");
-		JLabel nom = new JLabel("nom        :");
+		PlantePanel plantes = new PlantePanel();
 		
-		JTextField largeurText = new JTextField(10);
-		JTextField hauteurText = new JTextField(10);
-		JTextField nomText = new JTextField(10);
+		JLabel ensoleillement = new JLabel("Ensoleillement : ");
+		JLabel typeSol = new JLabel(" type de sol : ");
 		
-		JPanel hauteurPanel = new JPanel();
-		JPanel largeurPanel = new JPanel();
-		JPanel nomPanel = new JPanel();
 		
-		hauteurPanel.add(hauteur);
-		hauteurPanel.add(hauteurText);
+		JPanel soleilPanel = new JPanel();
+		JPanel solPanel = new JPanel();
 		
-		largeurPanel.add(largeur);
-		largeurPanel.add(largeurText);
-		
-		nomPanel.add(nom);
-		nomPanel.add(nomText);
-		
-		this.add(nomPanel);
-		this.add(hauteurPanel);
-		this.add(largeurPanel);
-				
 		JPanel resultPanel = new JPanel();
 		JButton annuler = new JButton("Annuler");
 		JButton valider = new JButton("Valider");
@@ -58,16 +39,11 @@ private static final long serialVersionUID = 1L;
 		
 		valider.addActionListener(e -> {
 			if (true) {
-				EditZoneFrame.this.res = new String[3];
-				EditZoneFrame.this.res[0] = nomText.getText();
-				EditZoneFrame.this.res[1] = largeurText.getText();
-				EditZoneFrame.this.res[2] = hauteurText.getText();
+				
 				EditZoneFrame.this.setVisible(false);
 				EditZoneFrame.this.dispose();
 			} else {
-				nomText.setText("");
-				largeurText.setText("");
-				hauteurText.setText("");
+				
 			}
 			});
 		
@@ -85,7 +61,7 @@ private static final long serialVersionUID = 1L;
 	 * @param f la fenetre qui cree
 	 * @return null ou un tableau de String avec les valeurs
 	 */
-	public static void showEditZoneFrame(JFrame f, AbstractZone z) {
-		new EditZoneFrame(f, z);
+	public static void showEditZoneFrame(JFrame f) {
+		new EditZoneFrame(f);
 	}
 }
