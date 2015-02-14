8274e6fa39682b3eb82ade0d0a8341fd1c06b259
Remy BOSQUART <remy.bosquart@etudiant.univ-reims.fr>
2015-02-14 09:28:41 +0100

    @bosq

--- src/jardin/AlgoSearch.java
+++ src/jardin/AlgoSearch.java
@@ -1,7 +1,9 @@
 package jardin;
 
 import jardin.ui.PlantothequeFrame;
+import jardin.Ensoleillement;
 
+import javax.swing.JFrame;
 import javax.swing.JList;
 
 import sun.swing.UIAction;
@@ -11,14 +13,30 @@ public class AlgoSearch {
 	private SortedListModel modelList = instance.getPlantes();
 	private JList listePlante;
 	
-	JList AlgoSearch(){
+	JList AlgoSearch(PlantothequeFrame pf){
+				
 		/////////Recherche par NOM
-		if(PlantothequeFrame.this.getNomF().getText() != null);
+		if(pf.getNomF().getText() != null);
 		
 		/////////Recherche par Famille
-		if(PlantothequeFrame.this.getFamilleF().getText() != null);
+		if(pf.getFamilleF().getText() != null);
 		
-		if(PlantothequeFrame.)
+		////////Recherche par Type de plante
+		if(pf.getTypePlanteComboBox().getSelectedItem() !=null);
+		
+		////////Recherche par type de sol
+		if(pf.getTypeSolComboBox().getSelectedItem() !=null);
+		
+		////////Recherche par ensoleillement
+		if(pf.getEnsoleillementComboBox().getSelectedItem() != null){
+			Ensoleillement e = (Ensoleillement) pf.getEnsoleillementComboBox().getSelectedItem();
+			for(int i=0;i<modelList.getSize();i++){
+				if(modelList.getElementAt(i).getEnsoleillement() == e.getValue()){
+					listePlante.add();
+				}
+			}
+		}
+		
 		
 		return this.listePlante;
 	}
